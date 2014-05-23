dcf60a5014e786300ab7926a82c86e9e286495c7
Michael DiStefano <m@simple.com>
2014-05-23 10:29:33 -0700

    Initial Commit

---
+++ MDCalendarDemo/MDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MDAppDelegate.h
+//  MDCalendarDemo
+//
+//  Created by Michael Distefano on 5/23/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
